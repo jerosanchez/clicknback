638d5507b8dd22e4257f35a798e69c04b22f2daa
Jero Sanchez <dev@jerosanchez.com>
2026-02-25 16:02:13 +0100

    Inject current user in create merchant endpoint

--- app/merchants/api.py
+++ app/merchants/api.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
+from app.core.current_user import get_current_admin_user
 from app.core.database import get_db
+from app.core.errors.builders import internal_server_error
+from app.core.logging import logging
 from app.merchants.composition import get_merchant_service
 from app.merchants.schemas import MerchantCreate, MerchantOut
 from app.merchants.services import MerchantService
+from app.users.models import User
 
 router = APIRouter(prefix="/api/v1")
 
@@ -14,10 +18,16 @@ def create_merchant(
     create_data: MerchantCreate,
     merchant_service: MerchantService = Depends(get_merchant_service),
     db: Session = Depends(get_db),
+    _current_user: User = Depends(get_current_admin_user),
 ) -> MerchantOut:
     try:
         new_merchant = merchant_service.create_merchant(create_data.model_dump(), db)
+
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+        logging.error(
+            "An unexpected error occurred while creating a merchant.",
+            extra={"error": str(e)},
+        )
+        raise internal_server_error()
 
     return new_merchant
